c6eb9458512c2a7b1f259d0e4166b46bb2f9d6fa
ninomsu <masternino5x@gmail.com>
2017-07-20 21:41:22 -0400

    Version 1.1
    Fixed login timeout issue

--- EtherMonitor.py
+++ EtherMonitor.py
@@ -3,10 +3,12 @@ import re
 import time
 import smtplib
 count = 0
-server = smtplib.SMTP( "smtp.gmail.com", 587 )
-server.starttls()
-server.login( 'Your Email', 'Your Password' )
-lastHourPrice = 100.0;
+response = ur.urlopen( "https://coinmarketcap.com/currencies/ethereum/")
+html = response.read()
+regex = '<span class="text-large" id="quote_price">(.+?)</span>'
+pattern = re.compile(regex)
+price =re.findall(pattern, html.decode('utf-8'))
+lastHourPrice = float(price[0][1:])
 
 while(True):
     response = ur.urlopen( "https://coinmarketcap.com/currencies/ethereum/")
@@ -19,13 +21,19 @@ while(True):
     
     #if the price of ether decreases by a min of 10% in the last hour, send text
     if priceNum < lastHourPrice:
-            if (1.0 - (priceNum/lastHourPrice)) >= .01:
+            if (1.0 - (priceNum/lastHourPrice)) >= .06:
+                server = smtplib.SMTP( "smtp.gmail.com", 587 )
+                server.starttls()
+                server.login( 'Your Email', 'Your Password' )
                 message = "Price of Ether = " + price[0] + "\n" + "Ether dropped " + str(round(100*(1.0 - (priceNum/lastHourPrice)), 2)) + "%"
                 server.sendmail( 'YOUREMAIL', 'YOURPHONENUMBER@mms.att.net', message )
 
     #if the price of ether increases by a min of 10% in the last hour, send text    
     else:
-        if (((priceNum - lastHourPrice)/lastHourPrice)) >= .01:
+        if (((priceNum - lastHourPrice)/lastHourPrice)) >= .06:
+                server = smtplib.SMTP( "smtp.gmail.com", 587 )
+                server.starttls()
+                server.login( 'Your Email', 'Your Password' )
                 message = "Price of Ether = " + price[0] + "\n" + "Ether went up " + str(round(100*(((priceNum - lastHourPrice)/lastHourPrice)), 2)) + "%"
                 server.sendmail( 'YOUREMAIL', 'YOURPHONENUMBER@mms.att.net', message )
         
